07c88dd784b864e49daee147d3ae1ffb59a7c075
nd-a <99365036+nd-a@users.noreply.github.com>
2022-02-09 13:19:52 -0800

    Add files via upload

---
+++ python.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 11:21:48 2022
+
+@author: n
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+
+URL = "https://www.cs.unc.edu/~saba/COMP_classes/index.html"
+page = requests.get(URL)
+x=(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+job_elements = list(soup.find_all('table'))
+table_MN = (pd.read_html("https://www.cs.unc.edu/~saba/COMP_classes/index.html"))[0].to_numpy()
+x=np.delete(table_MN,np.arange(1,len(table_MN),2),0)
+x.tofile('/Users/n/Downloads/sample1.csv',sep=',')
+    
+
